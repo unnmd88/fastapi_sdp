4b5845284b55f5d1d3050ba429acfa6f86800d0e
deb12 <unnmd88@gmail.com>
2025-02-15 18:50:55 +0300

    Добавлены тесты.

--- api_v1/tests/tests_services.py
+++ api_v1/tests/tests_services.py
@@ -24,6 +24,36 @@ def test_get_model_HostSorterSearchInDB():
     obj = services.HostSorterSearchInDB(model)
     assert obj.model_for_search_in_db == obj._get_model_for_search_in_db()
 
+def test_get_hosts_and_bad_hosts_as_dict_HostSorterSearchInDB():
+
+    fake_model = schemas.GetHostsStaticDataFromDb(hosts=['3230', '11', '10.45.154.16'])
+    obj = services.HostSorterSearchInDB(fake_model)
+
+    bad_hosts = [
+        {'string': {'entity': 'get_host_property', 'errors': ['not found in database']}},
+        {'abra': {'entity': 'get_host_property', 'errors': ['not found in database']}},
+        {'cadabra': {'entity': 'get_host_property', 'errors': ['not found in database']}}
+    ]
+    bad_hosts_dict = {
+        'string': {'entity': 'get_host_property', 'errors': ['not found in database']},
+        'abra': {'entity': 'get_host_property', 'errors': ['not found in database']},
+        'cadabra': {'entity': 'get_host_property', 'errors': ['not found in database']}
+    }
+
+    hosts = {
+        '10.179.28.9': {'number': '11', 'type_controller': 'Swarco',
+                         'address': 'Бережковская наб. д.22, 24    ЗАО (ЗАО-9)', 'description': 'Приоритет ОТ'},
+         '10.179.56.1': {'number': '12', 'type_controller': 'Поток (P)',
+                         'address': 'Щербаковская ул. - Вельяминовская ул. д.6к1,32   ВАО (ВАО-4)',
+                         'description': 'Приоритет ОТ'},
+         '10.179.40.9': {'number': '13', 'type_controller': 'Swarco',
+                         'address': 'Шереметьевская ул. д.60,62,29,27к1 - Марьиной Рощи 11-й пр-д (СВАО-2)',
+                         'description': None}
+    }
+
+    obj.bad_hosts, obj.hosts = bad_hosts, hosts
+    assert obj.get_bad_hosts_as_dict() == bad_hosts_dict
+    assert obj.get_hosts_and_bad_hosts_as_dict() == (hosts | bad_hosts_dict)
 
 
 if __name__ == '__main__':
