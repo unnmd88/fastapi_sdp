da47aee8cd86ba54182804b4d35db7c5f34fba0d
deb12 <unnmd88@gmail.com>
2025-02-18 22:50:37 +0300

    Реализация методов __repr__ и __setattr__ для базового класса Host в модуле controller_management2.py

--- sdp_lib/management_controllers/controller_management2.py
+++ sdp_lib/management_controllers/controller_management2.py
@@ -3,7 +3,7 @@
 from pysnmp.hlapi.asyncio import *
 
 from .snmp_oids import Oids
-
+from sdp_lib.utils_common import check_is_ipv4
 
 
 
@@ -69,6 +69,27 @@ class Host:
         self.scn = scn
         self.query_data = []
 
+    def __repr__(self):
+        return (
+            f'ip_v4: {self.ip_v4}\n'
+            f'host_id: {self.host_id}\n'
+            f'scn: {self.scn}'
+        )
+
+    def __setattr__(self, key, value):
+        if key == 'ip_v4':
+            if check_is_ipv4(key):
+                self.__dict__[key] = value
+            else:
+                raise ValueError(f'Значение < self.ipv4 > должно быть валидным ipv4 адресом: {value}')
+
+        elif key == 'scn':
+            if len(key) <= 10:
+                self.__dict__[key] = value
+            else:
+                raise ValueError('Значение < self.scn > не должно превышать 10 символов ')
+        else:
+            self.__dict__[key] = value
 
 
 
