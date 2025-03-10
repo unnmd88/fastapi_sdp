c62742ea3404658b0e53ba4104e84797336e1b8a
deb12 <unnmd88@gmail.com>
2025-03-10 18:28:46 +0300

    Реализован общий метод установки значений вводов для Peek web

--- sdp_lib/management_controllers/http/http_core.py
+++ sdp_lib/management_controllers/http/http_core.py
@@ -1,5 +1,7 @@
 import abc
 
+import aiohttp
+
 from sdp_lib.management_controllers.fields_names import FieldsNames
 from sdp_lib.management_controllers.hosts import Host
 
@@ -8,9 +10,10 @@ class HttpHost(Host):
 
     route: str
 
-    def __init__(self, ip_v4: str):
+    def __init__(self, ip_v4: str, session: aiohttp.ClientSession):
         super().__init__(ip_v4)
         self.base_url = f'http://{self.ip_v4}'
+        self.session = session
         # self.full_url = f'{self.base_url}{self.main_route}'
         self.parser = None
 
--- sdp_lib/management_controllers/http/peek/peek_web_management.py
+++ sdp_lib/management_controllers/http/peek/peek_web_management.py
@@ -18,11 +18,13 @@ load_dotenv()
 
 class SetData(PeekWeb):
 
-    # main_route: str
-
-    def __init__(self, ip_v4: str):
-        super().__init__(ip_v4=ip_v4)
+    def __init__(self, ip_v4: str, session: aiohttp.ClientSession):
+        super().__init__(ip_v4=ip_v4, session=session)
         self.get_data_from_web_page_obj: T = self.get_web_page_obj()
+
+    @abc.abstractmethod
+    def get_web_page_obj(self) -> T:
+        ...
 
     async def set_and_parse(
             self,
@@ -47,21 +49,62 @@ class SetData(PeekWeb):
         self.response = error, req_status
         return self
 
-    @abc.abstractmethod
-    def get_web_page_obj(self) -> T:
-        ...
+    async def create_tasks_and_send_request_to_set_val(
+            self,
+            session: aiohttp.ClientSession,
+            data_from_web: dict[str, str],
+            data_for_set: dict[str, str],
+            prefix: str,
+            index: int
+    ):
+        async with TaskGroup() as tg:
+            res = [
+                tg.create_task(
+                    self.set_and_parse(
+                        session=session,
+                        url=self.full_url,
+                        payload=self.get_payload(inp, val, data_from_web, prefix, index)
+                    )
+                )
+                    for inp, val in data_for_set.items()
+            ]
+        return res
 
+    def get_payload(
+            self,
+            inp_name,
+            val,
+            data,
+            prefix,
+            index
+    ):
+        param_index = data.get(inp_name)[index]
+        return {'par_name': f'{prefix}{param_index}', 'par_value': val}
 
 class SetInputs(SetData):
 
     route = routes.set_inputs
     prefix_inputs = os.getenv('INPUT_PREFIX_FOR_SET_VAL')
 
+
+    all_mpp_inputs = set(os.getenv('ALL_MPP_INPUTS').split())
+    mpp_stages_inputs = set(os.getenv('MPP_STAGES_INPUTS').split())
+
+    prefix_man_stage = os.getenv('PREFIX_MAN_STAGE_PEEK')
+    matches_name_inp_and_num_stage = {num: f'{prefix_man_stage}{num}' for num in range(1, 9)}
+
+
     INDEX, NUM, NAME, STATE, TIME, VALUE = range(6)
     RESULT = 1
 
     def get_web_page_obj(self) -> T:
-        return InputsPage(self.ip_v4)
+        return InputsPage(self.ip_v4, self.session)
+
+    async def get_data_and_set_response(self):
+        await self.get_data_from_web_page_obj.get_and_parse(session)
+        err, response = self.get_data_from_web_page_obj.response
+        if err is not None:
+            self.response = err, response
 
     async def set_any_vals(
             self,
@@ -73,34 +116,38 @@ class SetInputs(SetData):
         if err is not None:
             self.response = err, response
             return self
-        async with TaskGroup() as tg2:
-            res = [
-                tg2.create_task(
-                    self.set_and_parse(session=session, url=self.full_url, payload=self._get_payload(inp, val)))
-                    for inp, val in inps.items()
-            ]
-        if any(res_task.result().response[self.RESULT] != 200 for res_task in res):
+
+        result = await self.create_tasks_and_send_request_to_set_val(
+            session=session,
+            data_from_web=self.get_data_from_web_page_obj.parser.inputs_from_page,
+            data_for_set=inps,
+            prefix=self.prefix_inputs,
+            index=self.INDEX
+        )
+
+        if any(res_task.result().response[self.RESULT] != 200 for res_task in result):
             self.response = ErrorSetValue(), {}
             return self
         await self.get_data_from_web_page_obj.get_and_parse(session)
         self.response = self.get_data_from_web_page_obj.response
         return self
 
-    def _get_payload(self, inp_name, val, inp_index=None):
-        if inp_index is None:
-            inp_index = self.get_data_from_web_page_obj.parser.inputs_from_page.get(inp_name)[self.INDEX]
-        return {'par_name': f'{self.prefix_inputs}{inp_index}', 'par_value': val}
+    async def set_stage(self,session, value: int):
+        await self.get_data_from_web_page_obj.get_and_parse(session)
+        err, response = self.get_data_from_web_page_obj.response
+        if err is not None:
+            self.response = err, response
+            return self
+
 
 
 async def main():
-
-
 
     async with aiohttp.ClientSession() as sess:
         obj = SetInputs(ip_v4='10.179.112.241')
         # obj = SetInputs(ip_v4='10.45.154.19')
         # obj = SetInputs(ip_v4='10.179.20.9')
-        v = 1
+        v = 0
         inps = {
             'MPP_PH1': v,
             'MPP_PH2': v,
@@ -125,4 +172,5 @@ async def main():
 
 
 if __name__ == '__main__':
+
     rrr = asyncio.run(main())
