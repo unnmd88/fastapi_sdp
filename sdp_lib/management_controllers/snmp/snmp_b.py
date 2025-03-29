a93ef8bf6caee4e54f50db545848e8fae3444aea
deb12 <unnmd88@gmail.com>
2025-03-29 19:56:36 +0300

    Рефакторинг архитектуры в сервисе snmp, классы  ErrorResponseCheckers и SnmpRequests вынесены в отдельные модули

--- sdp_lib/management_controllers/snmp/snmp_b.py
+++ sdp_lib/management_controllers/snmp/snmp_b.py
@@ -16,8 +16,9 @@ from sdp_lib.management_controllers.parsers.snmp_parsers.ug405_parsers import Pa
 from sdp_lib.management_controllers.snmp.host_properties import swarco_stcip, potok_p, HostProtocolData
 # from sdp_lib.management_controllers.parsers.snmp_parsers.parsers_snmp_core import SwarcoStcipBase
 from sdp_lib.management_controllers.snmp.oids import Oids
+from sdp_lib.management_controllers.snmp.response_checkers import ErrorResponseCheckers
 from sdp_lib.management_controllers.snmp.response_structure import ResponseStructure
-
+from sdp_lib.management_controllers.snmp.snmp_requests import SnmpRequests
 
 # class SnmpHost(Host):
 #
@@ -257,92 +258,9 @@ from sdp_lib.management_controllers.snmp.response_structure import ResponseStruc
 
 """" """"" """" """""
 
-
-
-
-
-
-class SnmpRequests:
-
-    def __init__(self, ip, community_r, community_w, engine):
-        self.ip = ip
-        self.community_r = community_r
-        self.community_w = community_w
-        self.engine = engine
-
-    async def snmp_get(
-            self,
-            oids: list[str | Oids] | tuple[str | Oids, ...] | KeysView[str | Oids],
-            timeout: float = 1,
-            retries: int = 0
-    ) -> tuple[errind.ErrorIndication, Integer32 | int, Integer32 | int, tuple[ObjectType, ...]]:
-        """
-        Метод get запросов по snmp
-        :param ip_v4:
-        :param community: коммьюнити хоста
-        :param oids: список oids, которые будут отправлены в get запросе
-        :param timeout: таймаут запроса, в секундах
-        :param retries: количество попыток запроса
-        :return: tuple вида:
-                 index[0] -> если есть ошибка в snmp запросе, то текст ошибки, иначе None
-                 index[1] -> ответные данные. список вида [(oid, payload), (oid, payload)...]
-                 index[2] -> self, ссылка на объект
-
-        Examples
-        --------
-        ip_adress = '192.168.0.1'\n
-        community = 'community'\n
-        oids = [Oids.swarcoUTCTrafftechPhaseStatus,
-               Oids.swarcoUTCTrafftechPlanStatus]
-
-
-        asyncio.run(set_request(ip_adress, community, oids))
-        ******************************
-        """
-        print(f'oids: {oids}')
-        error_indication, error_status, error_index, var_binds = await get_cmd(
-            self.engine,
-            CommunityData(self.community_r),
-            await UdpTransportTarget.create((self.ip, 161), timeout=timeout, retries=retries),
-            ContextData(),
-            *[ObjectType(ObjectIdentity(oid)) for oid in oids]
-        )
-        # print(f'error_indication: {error_indication}\n'
-        #       f'error_status: {error_status}\n'
-        #       f'error_index: {error_index}\n'
-        #       f'var_binds: {var_binds}')
-        return error_indication, error_status, error_index, var_binds
-
-        # return self.check_response_and_add_error_if_has(error_indication, error_status, error_index), var_binds
-
-
-class ErrorResponseCheckers:
-
-    def __init__(
-            self,
-            host_instance
-    ):
-        self.host_instance = host_instance
-        # self.error_indication = self.response[ResponseStructure.ERROR_INDICATION]
-        # self.error_status = self.response[ResponseStructure.ERROR_STATUS]
-        # self.error_index = self.response[ResponseStructure.ERROR_INDEX]
-
-    def check_response_errors_and_add_to_host_data_if_has(self):
-
-        if self.host_instance.last_response[ResponseStructure.ERROR_INDICATION] is not None:
-            self.host_instance.add_data_to_data_response_attrs(self.host_instance.last_response[ResponseStructure.ERROR_INDICATION])
-        elif (
-            self.host_instance.last_response[ResponseStructure.ERROR_STATUS]
-            or self.host_instance.last_response[ResponseStructure.ERROR_INDEX]
-        ):
-            self.host_instance.add_data_to_data_response_attrs(BadControllerType())
-        return bool(self.host_instance.ERRORS)
-
-
 class SnmpHost(Host):
 
     protocol = FieldsNames.protocol_snmp
-
 
     host_properties: HostProtocolData
     type_controller: str
@@ -519,7 +437,6 @@ class PotokP(Ug405Host):
 
     async def get_states(self):
         return await self.make_get_request_and_parse_response(self.get_state_oids_state, self.states_parser)
-
 
 
 class SwarcoStcip(SnmpHost):
