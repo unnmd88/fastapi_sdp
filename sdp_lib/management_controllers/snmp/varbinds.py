569fc68b12a6e51c9e5fd1f50f565e3f4d297fc2
deb12 <unnmd88@gmail.com>
2025-04-06 15:52:10 +0300

    Реализация классов с данными varbinds для различных типов контроллеров: "Поток (S)", "Поток (P)", "Swarco". для каждого типа дк созданы соответствующие экземпляры(синглтоны) с данными varbinds

--- sdp_lib/management_controllers/snmp/varbinds.py
+++ sdp_lib/management_controllers/snmp/varbinds.py
@@ -17,7 +17,6 @@ from sdp_lib.management_controllers.snmp.oids import oids_scn_required
 from sdp_lib.management_controllers.snmp.oids import Oids
 
 
-
 def wrap_oid_by_object_type(oid, val: Unsigned32 | Integer | OctetString = None):
     return ObjectType(ObjectIdentity(oid), val or rfc1905.unSpecified)
 
@@ -37,14 +36,12 @@ def convert_val_to_num_stage_set_req(max_stage: int) -> dict:
     return {str(k): v for k, v in enumerate((f'{i}{j * "00"}' for j in range(max_stage//8) for i in stg_mask), 1)}
 
 
-ug405_stage_vaules = convert_val_to_num_stage_set_req(128)
-print(f'ug405_stage_vaules 1: {ug405_stage_vaules.get("8")}')
-pprint.pprint(ug405_stage_vaules)
-
+ug405_stage_values = convert_val_to_num_stage_set_req(128)
 
 
 T_OidsContainer = TypeVar('T_OidsContainer', list[Oids | str], tuple[Oids | str, ...])
 T_ObjectTypeContainer = TypeVar('T_ObjectTypeContainer', list[ObjectType], tuple[ObjectType, ...])
+
 
 class Varbinds(abc.ABC):
 
@@ -55,17 +52,17 @@ class Varbinds(abc.ABC):
 
     @abc.abstractmethod
     def _build_varbinds_for_get_state(self):
-        pass
+        ...
 
-    def get_varbinds_get_state(self):
-        raise NotImplementedError()
+    @abc.abstractmethod
+    def get_varbinds_current_states(self, *args, **kwargs):
+        ...
 
     @classmethod
     def parse_response(cls, varbinds):
         for oid, val in varbinds:
             print(f'oid: {oid.prettyPrint()} || val: {val.prettyPrint()}')
             print(f'oid as str: {str(oid)} || val: {val.prettyPrint()}')
-
 
 
 class VarbindsStcip(Varbinds):
@@ -79,11 +76,11 @@ class VarbindsStcip(Varbinds):
     def _build_varbinds_for_get_state(self) -> list[ObjectType]:
         return [wrap_oid_by_object_type(oid) for oid in self.oids_state]
 
+    def get_varbinds_current_states(self) -> list[ObjectType]:
+        return self._varbinds_get_state
+
     def _build_varbinds_for_set_stage(self) -> dict[int, ObjectType]:
         raise NotImplementedError()
-
-    def get_varbinds_get_state(self) -> list[ObjectType]:
-        return self._varbinds_get_state
 
     def get_varbinds_set_stage(self, num_stage) -> list[ObjectType]:
         return [self._varbinds_set_stage.get(num_stage)]
@@ -101,122 +98,15 @@ class VarbindsSwarco(VarbindsStcip):
              0: wrap_oid_by_object_type(Oids.swarcoUTCTrafftechPhaseCommand, 0)}
 
 
-# class VarbindsUg405(Varbinds):
-#
-#     max_scn = 9999
-#     num_CO_prefix = 'CO'
-#
-#     operation_mode2_varbind = wrap_oid_by_object_type(Oids.utcType2OperationMode, Integer(2))
-#     operation_mode3_varbind = wrap_oid_by_object_type(Oids.utcType2OperationMode, Integer(3))
-#     integer_vals = {i: Integer(i) for i in range(128)}
-#     hex_vals = {i: OctetString(str(i)) for i in range(128)}
-#
-#     def __init__(self):
-#         super().__init__()
-#         self._varbinds_get_state_all_scn = self._build_varbinds_for_get_state()
-#
-#     def _add_scn_to_oids(
-#             self,
-#             scn: str,
-#             oids: T_OidsContainer,
-#             wrap_oid_by_object_identity: bool = False
-#     ) -> list[Oids | str | ObjectType]:
-#
-#         if wrap_oid_by_object_identity:
-#             return [
-#                 ObjectType(ObjectIdentity(f'{oid}{scn}')) if oid in oids_scn_required
-#                 else ObjectType(ObjectIdentity(oid))
-#                 for oid in oids
-#             ]
-#         return [
-#             f'{oid}{scn}' if oid in oids.oids_scn_required else oid
-#             for oid in oids
-#         ]
-#
-#     def add_scn_to_single_oid(
-#             self,
-#             scn: str,
-#             oid: Oids,
-#             wrap_oid_by_object_identity: bool = False
-#     ) -> Oids | str | ObjectType:
-#
-#         oid = f'{oid}{scn}' if oid in oids.oids_scn_required else oid
-#         if wrap_oid_by_object_identity:
-#             return wrap_oid_by_object_type(oid)
-#         return  oid
-#
-#     def _build_operation_mode_varbinds(self, mode: int):
-#         return wrap_oid_by_object_type(Oids.utcType2OperationMode, self.integer_vals.get(mode))
-#
-#     def _build_varbinds_for_get_state(self):
-#         start_time = time.time()
-#
-#         result = {}
-#         for i in range(1, self.max_scn):
-#             scn = convert_chars_string_to_ascii_string(f'{self.num_CO_prefix}{str(i)}')
-#             curr_states_object_type = self.add_scn_to_oids_and_wrap_oids_by_object_identity(
-#                 scn, self.oids_state
-#             )
-#             result[scn] = curr_states_object_type
-#         print(f'FinS: {time.time() - start_time}')
-#         return result
-#
-#     def build_set_stage_oids_by_scn(self, scn_as_ascii):
-#
-#         res = {}
-#         for i in range(1, self.max_stage):
-#         # for i in range(1, 3):
-#             op_mode = wrap_oid_by_object_type(Oids.utcType2OperationMode, Integer(3))
-#
-#             TO_bit = wrap_oid_by_object_type(
-#                 self.add_scn_to_single_oid(scn_as_ascii, Oids.utcControlTO), Integer(1)
-#             )
-#             Fn_bit = wrap_oid_by_object_type(
-#                 self.add_scn_to_single_oid(scn_as_ascii, Oids.utcControlFn), OctetString(hexValue=str(i))
-#             )
-#             res[i] = (op_mode, TO_bit, Fn_bit)
-#         return res
-#
-#     def _build_varbinds_for_set_stage(self):
-#         start_time = time.time()
-#
-#         result = {}
-#         for i in range(1, self.max_scn):
-#             start_time = time.time()
-#
-#             scn = convert_chars_string_to_ascii_string(f'{self.num_CO_prefix}{str(i)}')
-#             result[scn] = self.build_set_stage_oids_by_scn(scn)
-#             print(f'one iteration : {time.time() - start_time}')
-#
-#         print(f'final_time: {time.time() - start_time}')
-#
-#         return result
-#
-#     def add_scn_to_oids(
-#             self,
-#             scn: str,
-#             oids: T_OidsContainer
-#     ) -> T_OidsContainer:
-#         return self._add_scn_to_oids(scn, oids)
-#
-#     def add_scn_to_oids_and_wrap_oids_by_object_identity(
-#             self,
-#             scn: str,
-#             oids: T_OidsContainer
-#     ) -> T_ObjectTypeContainer:
-#         return self._add_scn_to_oids(scn, oids, wrap_oid_by_object_identity=True)
-#
-#     def get_varbinds_get_state_by_scn(self, scn_as_ascii: str) -> T_ObjectTypeContainer:
-#         return self._varbinds_get_state_all_scn.get(scn_as_ascii)
-#
-#     def get_varbinds_set_stage(self, scn_as_ascii, num_stage):
-#         return self._varbinds_set_stage.get(scn_as_ascii).get(num_stage)
-#
-#     def _build_hex_vals_stages(self):
-#         return {num_stage: OctetString(hexValue=str(num_stage)) for num_stage in range(1, 129)}
-#
-#     def get_stage_as_hex(self, num_stage_as_integer):
-#         return self._hex_values_stages.get(num_stage_as_integer, OctetString(hexValue='0'))
+class VarbindsPotokS(VarbindsStcip):
+
+    oids_state = oids.oids_state_potok_s
+
+    def _build_varbinds_for_set_stage(self) -> dict[int, ObjectType]:
+        return {
+            num_stage: wrap_oid_by_object_type(Oids.swarcoUTCTrafftechPhaseCommand, Unsigned32(num_stage + 1))
+            for num_stage in range(1, 129)
+        } | {0: wrap_oid_by_object_type(Oids.swarcoUTCTrafftechPhaseCommand, 0)}
 
 
 class VarbindsUg405(Varbinds):
@@ -227,12 +117,13 @@ class VarbindsUg405(Varbinds):
     operation_mode1_varbind = wrap_oid_by_object_type(Oids.utcType2OperationMode, Integer32(1))
     operation_mode2_varbind = wrap_oid_by_object_type(Oids.utcType2OperationMode, Integer32(2))
     operation_mode3_varbind = wrap_oid_by_object_type(Oids.utcType2OperationMode, Integer32(3))
-    hex_vals128 = {i: OctetString(hexValue=ug405_stage_vaules.get(str(i))) for i in range(1, 129)}
+
+    hex_vals128 = {i: OctetString(hexValue=ug405_stage_values.get(str(i))) for i in range(1, 129)}
+
     integer_vals128 = {i: Integer32(i) for i in range(129)}
     integer32_val1 = Integer32(1)
     integer32_val2 = Integer32(2)
     integer32_val3 = Integer32(3)
-
 
     def add_scn_to_oids(
             self,
@@ -291,16 +182,15 @@ class VarbindsPotokP(VarbindsUg405):
 
     oids_state = oids.oids_state_potok_p
 
-
-potok_ug405 = VarbindsPotokP()
-
-
-
+# Singleton instances
+potok_ug405_varbinds = VarbindsPotokP()
+potok_stcip_varbinds = VarbindsPotokS()
+swarco_sctip_varbinds = VarbindsSwarco()
 
 
 class AbstractVarbindsWrappersByScn:
 
-    _ug405_varbinds: potok_ug405
+    _ug405_varbinds: potok_ug405_varbinds
 
     def __init__(self, scn_as_ascii: str = None):
         self._scn_as_ascii = scn_as_ascii
@@ -346,8 +236,7 @@ class AbstractVarbindsWrappersByScn:
 
 class WrappersVarbindsByScnPotokP(AbstractVarbindsWrappersByScn):
 
-
-    _ug405_varbinds = potok_ug405
+    _ug405_varbinds = potok_ug405_varbinds
 
 
 
@@ -387,18 +276,28 @@ async def main():
         community_w=host_data.potok_p.community_w,
         engine=SnmpEngine()
     )
-    scn2600 = convert_chars_string_to_ascii_string('CO2600')
-    # res = await r_sender.snmp_get(varbinds=potok_ug405.get_varbinds_current_states(scn155))
-    obj = WrappersVarbindsByScnPotokP(scn2600)
-    # res = await r_sender.snmp_get(varbinds=obj.get_varbinds_current_states())
-    print(f's: {obj.ug405_varbinds.get_varbinds_current_states(scn2600)}')
-    print(f's: {obj.ug405_varbinds._varbinds_get_state.get(scn2600)}')
-    res = await r_sender.snmp_get(varbinds=obj.ug405_varbinds.get_varbinds_current_states(scn2600))
-    print(res)
-    print(VarbindsSwarco.parse_response(res[3]))
 
-    res2 = await r_sender.snmp_set(varbinds=obj.get_varbinds_set_stage(0))
-    print(VarbindsSwarco.parse_response(res2[3]))
+    r_sender = snmp_requests.SnmpRequests(
+        # ip='10.179.56.105',
+        ip='10.179.69.65',
+        community_r=host_data.potok_p.community_r,
+        community_w=host_data.potok_p.community_w,
+        engine=SnmpEngine()
+    )
+
+    r_sender = snmp_requests.SnmpRequests(
+        # ip='10.179.56.105',
+        ip='10.179.68.177',
+        community_r=host_data.potok_s.community_r,
+        community_w=host_data.potok_s.community_w,
+        engine=SnmpEngine()
+    )
+
+    res = await r_sender.snmp_get(potok_stcip_varbinds.get_varbinds_current_states())
+
+    print(res)
+
+    potok_stcip_varbinds.parse_response(res[3])
 
 if __name__ == '__main__':
     asyncio.run(main())
