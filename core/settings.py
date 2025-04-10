e9e72f34284c0ca74a4ef6421de1a694b288b44d
deb12 <unnmd88@gmail.com>
2025-04-10 08:07:35 +0300

    Настройки старта приложения вынесены в settings.py

--- core/settings.py
+++ core/settings.py
@@ -1,10 +1,17 @@
 import os
 from pathlib import Path
 
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+class RunApp(BaseModel):
+    host: str = '127.0.0.1'
+    port: int = 8000
+    reload: bool = True
 
 
 class SettingsDb(BaseSettings):
@@ -34,6 +41,8 @@ class Settings(BaseSettings):
     traffic_lights_tag_monitoring: str = 'Traffic lights monitoring'
     traffic_lights_tag_management: str = 'Traffic lights management'
 
+    run_config_default: RunApp = RunApp()
+    run_config_sdp: RunApp = RunApp(host='192.168.45.93', port=8001)
 
 settings_db = SettingsDb()
 settings = Settings()
