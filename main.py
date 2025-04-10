c3e4cbbeddc8cde83b834d637ed9d51afb8d048a
deb12 <unnmd88@gmail.com>
2025-04-10 08:11:38 +0300

    PEP8

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ from core.models import Base, db_helper
 from api_v1 import router as router_v1
 from core.settings import settings
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     async with db_helper.engine.connect() as conn:
         await conn.run_sync(Base.metadata.create_all)
     yield
+
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
@@ -19,9 +21,8 @@ app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 
 @app.get('/')
 def root():
-    print(f'this:')
     return {
-        'this': 'main_page'
+        'Message': 'Hello from monitoring and management api'
     }
 
 if __name__ == '__main__':
